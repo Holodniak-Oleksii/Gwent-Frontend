3933ea1b2b97de8e06277ea048e29c7a32676da2
Holodniak-Oleksii <arsak13.1881@gmail.com>
2025-02-02 14:15:09 +0200

    GF-game complete base game logic

--- src/features/Arena/states/End/End.tsx
+++ src/features/Arena/states/End/End.tsx
@@ -4,10 +4,18 @@ import { useUserStore } from "@/store/user";
 export const End = () => {
   const gameStore = useGameStore((state) => state.game);
   const user = useUserStore((state) => state.user);
+
+  const result =
+    gameStore?.winner === "draw"
+      ? "Draw"
+      : user?.nickname === gameStore?.winner
+      ? "You Win"
+      : "You Lose";
+
   return (
     <div>
       Game End <br />
-      You {user?.nickname === gameStore?.winner ? "Win" : "Lose"}
+      {result}
     </div>
   );
 };
