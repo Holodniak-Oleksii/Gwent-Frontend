a3781bbcb85f8a5c0583deaa21a132a129c9764a
Holodniak-Oleksii <arsak13.1881@gmail.com>
2025-02-02 13:58:48 +0200

    GF-game complete base game logic

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -12,5 +11,9 @@ export default defineConfig({
   },
   server: {
     open: true,
+  },
+  base: "/",
+  build: {
+    outDir: "dist",
   },
 });
