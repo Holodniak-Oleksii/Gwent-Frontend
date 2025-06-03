032e69a939c3ac4fb5bd06f11b8783d1346dfb0b
Holodniak-Oleksii <arsak13.1881@gmail.com>
2025-06-03 09:54:37 +0300

    update markets

--- src/store/filters/data.ts
+++ src/store/filters/data.ts
@@ -2,13 +2,13 @@ import { IFilterFields } from "./types";
 
 export const DEFAULT_FIELDS: IFilterFields = {
   CARDS: {
-    ability: [],
-    force: [],
-    fraction: [],
+    fractions: [],
+    abilities: [],
+    forces: [],
+    types: [],
     name: "",
     page: 1,
     size: 18,
-    type: [],
   },
   PLAYERS: {
     page: 1,
